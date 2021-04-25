bf5d88076098c4e33f53d1d38d7c75f785d1474f
maxfri <maxfri1995@gmail.com>
2021-04-25 23:48:58 +0300

    feat: add output value

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,10 +1,12 @@
 const fullscreen = document.querySelector(".fullscreen");
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll("input");
 
 function handleUpdate() {
-  const suffix = this.dataset.sizing || '';
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-  console.log(this.name);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
 function getFullscreen() {
@@ -17,7 +19,26 @@ function getFullscreen() {
   }
 }
 
+const fileInput = document.querySelector('input[type="file"]');
+const imageContainer = document.querySelector("img");
+
+fileInput.addEventListener("change", function (e) {
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    const img = new Image();
+    img.src = reader.result;
+    imageContainer.innerHTML = "";
+    imageContainer.append(img);
+  };
+  reader.readAsDataURL(file);
+});
+
 fullscreen.addEventListener("click", getFullscreen);
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach((input) => input.addEventListener("input", handleUpdate));
+inputs.forEach((input) =>
+  input.addEventListener("input", () => {
+    input.nextElementSibling.value = input.value;
+  })
+);
