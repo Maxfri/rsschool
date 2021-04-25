75283a520edc861210cbd09ee57763a472b672c5
maxfri <maxfri1995@gmail.com>
2021-04-26 01:03:13 +0300

    feat: add night images

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -2,7 +2,8 @@ const fullscreen = document.querySelector(".fullscreen");
 const inputs = document.querySelectorAll("input");
 const fileInput = document.querySelector('input[type="file"]');
 const imageContainer = document.querySelector("img");
-const reset = document.querySelector(".btn-reset")
+const reset = document.querySelector(".btn-reset");
+const next = document.querySelector(".btn-next");
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || "";
@@ -56,3 +57,24 @@ inputs.forEach((input) =>
     input.nextElementSibling.value = input.value;
   })
 );
+
+
+const base = './assets/img/night/';
+const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+let i = 0;
+const img = document.querySelector('img');
+
+function viewBgImage(src) {  
+  img.src = src;
+  img.onload = () => {      
+    img.src = `${src}`;
+  }; 
+}
+
+function getImage() {
+  const index = i % images.length;
+  const imageSrc = base + images[index];
+  viewBgImage(imageSrc);
+  i++;
+} 
+next.addEventListener('click', getImage);
