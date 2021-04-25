d44270eaa7fcdd84b11cea8c00f2ffd11a97ae7f
maxfri <maxfri1995@gmail.com>
2021-04-25 11:23:09 +0300

    init: srart task photo-filter

---
+++ photo-filter/script.js
@@ -0,0 +1,11 @@
+fullscreen.addEventListener("click", getFullscreen);
+
+function getFullscreen() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  }
+
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  }
+}
