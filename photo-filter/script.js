74b4b30a6240be0946d97fa959210104f30072af
maxfri <maxfri1995@gmail.com>
2021-04-26 23:28:47 +0300

    feat: add time of day

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,7 +1,7 @@
 const fullscreen = document.querySelector(".fullscreen");
 const inputs = document.querySelectorAll("input");
 const fileInput = document.querySelector('input[type="file"]');
-const imageContainer = document.querySelector("img");
+const imageContainer = document.querySelector(".editor");
 const reset = document.querySelector(".btn-reset");
 const next = document.querySelector(".btn-next");
 
@@ -49,32 +49,76 @@ fileInput.addEventListener("change", function (e) {
 
 fullscreen.addEventListener("click", getFullscreen);
 reset.addEventListener("click", () => {
-  inputs.forEach((input) => resetValue(input))
-})
+  inputs.forEach((input) => resetValue(input));
+});
 inputs.forEach((input) => input.addEventListener("input", handleUpdate));
 inputs.forEach((input) =>
   input.addEventListener("input", () => {
-    input.nextElementSibling.value = input.value;
+    if (input.name !== "upload") {
+      input.nextElementSibling.value = input.value;
+    }
   })
 );
 
-
-const base = './assets/img/night/';
-const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+const base =
+  "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/";
+const images = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg",
+  "08.jpg",
+  "09.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+  "19.jpg",
+  "20.jpg",
+];
 let i = 0;
-const img = document.querySelector('img');
+const img = document.querySelector("img");
 
-function viewBgImage(src) {  
+function viewBgImage(src) {
   img.src = src;
-  img.onload = () => {      
+  img.onload = () => {
     img.src = `${src}`;
-  }; 
+  };
+}
+
+function getDate() {
+  const now = new Date();
+  let hours = now.getHours();
+  // let minutes = now.getMinutes();
+  // let seconds = now.getSeconds();
+
+  if (hours > 6 && hours < 12) {
+    timeOfDay = "morning/";
+  } else if (hours > 12 && hours < 18) {
+    timeOfDay = "day/";
+  } else if (hours > 18 && hours < 24) {
+    timeOfDay = "evening/";
+  } else if (hours > 0 && hours < 6) {
+    timeOfDay = "night/";
+  }
+  console.log(timeOfDay);
+  return timeOfDay;
 }
 
 function getImage() {
   const index = i % images.length;
-  const imageSrc = base + images[index];
+  const today = getDate();
+  const imageSrc = base + today + images[index];
   viewBgImage(imageSrc);
   i++;
-} 
-next.addEventListener('click', getImage);
+}
+next.addEventListener("click", getImage);
+
+getDate();
