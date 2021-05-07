75074be2cdf3ed453aceb546234424b4ed9dc6dd
maxfri <maxfri1995@gmail.com>
2021-05-07 04:40:44 +0300

    feat: add map slider

--- online-zoo/mainSlider.js
+++ online-zoo/mainSlider.js
@@ -16,12 +16,9 @@ function selectItem(item) {
   widthLeft = 187;
   let moveLeft = (item.dataset.id - 2) * widthLeft;
   mainGallerylist.style.left = `${-moveLeft}px`;
-
-  for (let i = 0; i < mainGalleryItems.length; i++) {
-    mainGalleryItems[i].classList.remove("main-gallery__active-item");
-    console.log(mainGalleryItems[i].childNodes[1]);
-    mainGalleryItems[i].childNodes[1].classList.remove("pets__info-hover");
-  }
+  mainGalleryItems.forEach((items) => {
+    items.classList.remove("main-gallery__active-item");
+  });
 
   mainGalleryItems[item.dataset.id - 1].classList.add(
     "main-gallery__active-item"
@@ -29,7 +26,6 @@ function selectItem(item) {
   mainGalleryItems[item.dataset.id - 1].childNodes[1].classList.add(
     "pets__info-hover"
   );
-
   mainPaginator.value = item.dataset.id;
   mainPaginatorNumber.innerHTML = `0${mainPaginator.value}/`;
 }
@@ -38,11 +34,9 @@ mainPaginator.addEventListener("input", function () {
   widthLeft = 187;
   let moveLeft = (mainPaginator.value - 2) * widthLeft;
   mainGallerylist.style.left = `${-moveLeft}px`;
-
-  for (let i = 0; i < mainGalleryItems.length; i++) {
-    mainGalleryItems[i].classList.remove("main-gallery__active-item");
-    mainGalleryItems[i].childNodes[1].classList.remove("pets__info-hover");
-  }
+  mainGalleryItems.forEach((items) => {
+    items.classList.remove("main-gallery__active-item");
+  });
   mainGalleryItems[mainPaginator.value - 1].classList.add(
     "main-gallery__active-item"
   );
