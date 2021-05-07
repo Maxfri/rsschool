aedde823a05025ada674be277c6cc7570aa14e24
maxfri <maxfri1995@gmail.com>
2021-05-07 10:14:58 +0300

    feat: add left and right click for map slider

--- online-zoo/pages/map/mapSlider.js
+++ online-zoo/pages/map/mapSlider.js
@@ -11,28 +11,51 @@ const mapLinks = [
 ];
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
+let number = mapPaginator.value;
+
 mapTooltips.forEach((tooltip) =>
   tooltip.addEventListener("click", () => pickTooltip(tooltip))
 );
 
-rightArrow.addEventListener("click", () => {
+rightArrow.addEventListener("click", () => pickRightArrow());
+leftArrow.addEventListener("click", () => pickLeftArrow());
+
+mapPaginator.addEventListener("input", () => {
   mapGallaryCards.forEach((card) => {
     card.classList.remove("gallery__card-active");
   });
+  let number = mapPaginator.value;
+  mapGallaryCards[number - 1].classList.add("gallery__card-active");
+  mapPaginatorNumber.innerHTML = `0${number}/`;
 });
 
-  mapPaginator.addEventListener("input", () => {
-    mapGallaryCards.forEach((card) => {
-      card.classList.remove("gallery__card-active");
-    });
-    let number = mapPaginator.value;
-    mapGallaryCards[number - 1].classList.add("gallery__card-active");
-    mapPaginatorNumber.innerHTML = `0${number}/`;
+function pickRightArrow() {
+  mapGallaryCards.forEach((card) => {
+    card.classList.remove("gallery__card-active");
   });
+  number++;
+  if (number > 8) {
+    number = 1;
+  }
+  mapGallaryCards[number - 1].classList.add("gallery__card-active");
+  mapPaginatorNumber.innerHTML = `0${number}/`;
+  mapPaginator.value = mapGallaryCards[number -1].dataset.id;
+}
 
+function pickLeftArrow() {
+  mapGallaryCards.forEach((card) => {
+    card.classList.remove("gallery__card-active");
+  });
+  number--;
+  if (number < 1) {
+    number = 8;
+  }
+  mapGallaryCards[number - 1].classList.add("gallery__card-active");
+  mapPaginatorNumber.innerHTML = `0${number}/`;
+  mapPaginator.value = mapGallaryCards[number -1].dataset.id;
+}
 
 function pickTooltip(tooltip) {
-  // slider.style.transform = `translateX(calc(${0}%))`;
   mapTooltips.forEach((tooltips) => {
     tooltips.classList.remove("map__tooltip-hover");
   });
@@ -44,11 +67,4 @@ function pickTooltip(tooltip) {
     card.classList.remove("gallery__card-active");
   });
   mapGallaryCards[tooltip.dataset.id - 1].classList.add("gallery__card-active");
-  // for (let i = 0; i < sliderMap.length; i++) {
-  //   sliderMap[i].classList.remove("gradient-round-border");
-  //   sliderMap[i].childNodes[0].classList.remove("selected");
-  // }
-  // sliderMap[num.value - 1].classList.add("gradient-round-border");
-  // sliderMap[num.value - 1].childNodes[0].classList.add("selected");
-  // position = num.value - 1;
 }
