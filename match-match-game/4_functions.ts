ac8e24ae6a37be6f6266259dc0579f641dbdf0da
maxfri <maxfri1995@gmail.com>
2021-05-20 10:34:31 +0300

    feat: start new routing

--- match-match-game/4_functions.ts
+++
@@ -1,8 +0,0 @@
-function add(a: number, b: number): number {
-  return a + b
-}
-
-function toUpperCase(str: string): string {
-  return str.trim().toUpperCase()
-}
-
--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,7 +1,7 @@
 import { CounterServiceImplmentation } from './app/counter.service';
 import './style.scss';
 import { App } from './app/app';
-// import { Router } from './app/router';
+import { Router } from './app/router';
 
 const counterService = new CounterServiceImplmentation();
 
@@ -14,6 +14,31 @@ window.onload = () => {
   if (!appElement) throw Error('App root element not found');
   // new App(appElement, counterService).render();
   new App(appElement).start();
+
+    // инициализируем роутер
+    Router.init();
+       
+    // запускаем главную страницу
+    Router.dispatch('/');
+
+    // обработчик нажатий на ссылки
+    let handler = event =>  {
+       
+        // получаем запрошенный url
+        let url = new URL(event.currentTarget.href);
+       
+        // запускаем роутер, предавая ему path
+        Router.dispatch(url.pathname);
+       
+        // запрещаем дальнейший переход по ссылке
+        event.preventDefault();
+    }
+
+    // получаем все ссылки на странице
+    let anchors = document.querySelectorAll('a');
+   
+    // вешаем на событие onclick обработчик
+    for( let anchor of anchors ) anchor.onclick = handler;
 };
 
 // const router = new Router({
