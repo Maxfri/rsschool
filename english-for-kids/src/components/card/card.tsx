f95a30bd16d7bdccc9ae323d33336ac6d652248a
maxfri <maxfri1995@gmail.com>
2021-06-30 01:53:34 +0300

    feat: add cards image and card component

--- english-for-kids/src/components/card/card.tsx
+++ english-for-kids/src/components/card/card.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-// import '../card/card.css';
+import cards from './CardsData';
+import './card.css';
 
 function Card(): JSX.Element {
   return (
     <>
-      <div className="card">
-        <img className="img" src="../src/assets/img/image7.png" alt="Card image" />
-        <div className="card-body">
-          <p className="card-text" />
-          <img className="card-flip" src="../src/assets/img/cached_black.png" alt="Card flip" />
+      {cards.map((item, index) => (
+        <div className="card">
+          <img className="img" src={item.image} alt="Card image" />
+          <div className="card-body">
+            <p className="card-text" />
+            {item.word}
+            <img className="card-flip" src="../src/assets/img/cached_black.png" alt="Card flip" />
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
